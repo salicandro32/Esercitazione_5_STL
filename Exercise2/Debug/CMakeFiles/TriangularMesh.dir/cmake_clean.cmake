545a1414a8771f6b45f7880bc5bf961d7537608b
salicandro32 <s307590@studenti.polito.it>
2025-04-28 23:34:15 +0200

    Restored all files

---
+++ Exercise2/Debug/CMakeFiles/TriangularMesh.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TriangularMesh.dir/ExportParaview/UCDUtilities.cpp.o"
+  "CMakeFiles/TriangularMesh.dir/ExportParaview/UCDUtilities.cpp.o.d"
+  "CMakeFiles/TriangularMesh.dir/main.cpp.o"
+  "CMakeFiles/TriangularMesh.dir/main.cpp.o.d"
+  "CMakeFiles/TriangularMesh.dir/src/Utils.cpp.o"
+  "CMakeFiles/TriangularMesh.dir/src/Utils.cpp.o.d"
+  "TriangularMesh"
+  "TriangularMesh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TriangularMesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
